968f0ac529b067ca5802ced7dc1f61d76cf8b312
Iris Moini <azadeh@umail.ucsb.edu>
2020-05-17 18:39:22 -0700

    minor fixes made to update

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,7 +1,9 @@
-import React, { Component} from 'react';
-import { Alert } from "react-native";
-
+import React, { Component } from 'react';
+import { motion } from "framer-motion";
+import { View, StyleSheet, Button, Alert } from "react-native";
+import { useAlert } from 'react-alert'
 import config from '../config.js';
+
 const firebase = require('firebase');
 
 
@@ -17,6 +19,8 @@ export class Form extends Component {
                 visibility: "", 
                 email: ""
             },
+
+            items: [],
         
         }
             
@@ -34,12 +38,15 @@ export class Form extends Component {
     handleSubmit(e) {
         e.preventDefault();
         const itemsRef = firebase.database().ref('data');
+        var dateTime = new Date().toLocaleString();
         const item = {
             name: this.state.name,
             description: this.state.description,
             message: this.state.message,  
             visibility: this.state.visibility, 
-            email: this.state.email
+            email: this.state.email,
+            startedAt: dateTime
+
         
         }
 
@@ -47,7 +54,7 @@ export class Form extends Component {
   
         itemsRef.push(item);
        
-        Alert.alert('Form was Successfull Summited!');
+        alert('Form was Successfull Summited!');
 
         this.setState({
             name: "", 
@@ -60,40 +67,46 @@ export class Form extends Component {
       }
    
 
-    render(){
-    return (
-        <form onSubmit={this.handleSubmit}>
-            <h2>Form</h2>
-            <div>
-                <p>What is you name?</p>
-                <input type="text" name="name" onChange={this.handleChange} value={this.state.name} minLength='5' maxLength='20' required/>
-            </div>
-
-            <div>
-                <p>Offer a short description of yourself.</p>
-                <input type="text" name="description" onChange={this.handleChange} maxLength='100' value={this.state.description} />
-            </div>
-
-            <div>
-                <p>What is your message?</p>
-                <input type="text" name="message" onChange={this.handleChange} value={this.state.message} minLength='15' maxLength='500' required/>
-            </div>
-
-            <div>
-                <p>Would you like your message to be private?</p>
-                    <input type="text" name="visibility" onChange={this.handleChange} value={this.state.visibility} required/>
-            </div>
-
-            <div>
-                <p>What is your email?</p>
-                <input type="text" name="email" onChange={this.handleChange} value={this.state.email} />
-            </div>
-            <button>Summit Form</button>
-        </form>
-
-
-    )
-    }
+    
+        render() {
+            return (
+                <form className='formStyle' onSubmit={this.handleSubmit}>
+    
+                    <h2 className='formElements'>Submission Form</h2>
+                    <div className='formElements'>
+                        <p> What is you name?</p>
+                        <input type="text" name="name" onChange={this.handleChange} value={this.state.name} minLength='5' maxLength='20' required />
+                    </div>
+    
+                    <div className='formElements'>
+                        <p>Offer a short description of yourself.</p>
+                        <input type="text" name="description" onChange={this.handleChange} maxLength='100' value={this.state.description} />
+                    </div>
+    
+                    <div className='formElements'>
+                        <p>What is your message?</p>
+                        <input type="text" name="message" onChange={this.handleChange} value={this.state.message} minLength='15' maxLength='500' required />
+                    </div>
+    
+                    <div className='formElements'>
+                        <p>Would you like your message to be private?</p>
+                        <select name="visibility" onChange={this.handleChange} value={this.state.visibility} required='true'>
+                            <option value="" selected disabled hidden> Select an Option </option>
+                            <option value="Yes">Yes</option>
+                            <option value="No">No</option>
+                        </select>
+                    </div>
+    
+                    <div className='formElements'>
+                        <p>What is your email?</p>
+                        <input type="text" name="email" onChange={this.handleChange} value={this.state.email} />
+                    </div>
+    
+                    <button className='formButton'>Submit Form</button>
+                </form>
+            )
+        }
+    
   
 }
 export default Form;
