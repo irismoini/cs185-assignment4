c239189bf50f12d6bad3da6da75ce021e91b5a4f
Iris Moini <azadeh@umail.ucsb.edu>
2020-06-07 22:33:44 -0700

    Updates

---
+++ precache-manifest.f262bf18f8164b3d2416b86b77d267cf.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4ddb2433206d61567c21c9896074f46",
+    "url": "/cs185-assignment4/index.html"
+  },
+  {
+    "revision": "07f33aaf711166e790b7",
+    "url": "/cs185-assignment4/static/css/2.5db8a242.chunk.css"
+  },
+  {
+    "revision": "8cafc553e630956f28e9",
+    "url": "/cs185-assignment4/static/css/main.55639a1a.chunk.css"
+  },
+  {
+    "revision": "07f33aaf711166e790b7",
+    "url": "/cs185-assignment4/static/js/2.05e23bbe.chunk.js"
+  },
+  {
+    "revision": "9bf6a815aabaa7f0ec01f29182401e3f",
+    "url": "/cs185-assignment4/static/js/2.05e23bbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cafc553e630956f28e9",
+    "url": "/cs185-assignment4/static/js/main.b7ab689e.chunk.js"
+  },
+  {
+    "revision": "a203224a32501e869884",
+    "url": "/cs185-assignment4/static/js/runtime-main.3b62a101.js"
+  },
+  {
+    "revision": "79cfe3997328938598d8d11d85019df0",
+    "url": "/cs185-assignment4/static/media/f1.79cfe399.jpg"
+  },
+  {
+    "revision": "5da03b61e7ee22137184613cfbae567c",
+    "url": "/cs185-assignment4/static/media/f4.5da03b61.jpg"
+  },
+  {
+    "revision": "c0e2a8186cd115922389dc2c058e6508",
+    "url": "/cs185-assignment4/static/media/f5.c0e2a818.jpg"
+  },
+  {
+    "revision": "3fe62ba31a64efa3d7e6c31f40da8384",
+    "url": "/cs185-assignment4/static/media/l1.3fe62ba3.jpg"
+  },
+  {
+    "revision": "504d0440f477daad5136b819e7c5470e",
+    "url": "/cs185-assignment4/static/media/lan5.504d0440.jpg"
+  },
+  {
+    "revision": "a770b6797b68e3f8920e473eb824bac0",
+    "url": "/cs185-assignment4/static/media/loader-big.a770b679.gif"
+  },
+  {
+    "revision": "071f28a4e8035a2f4c6ddd950a88897a",
+    "url": "/cs185-assignment4/static/media/p2.071f28a4.jpg"
+  },
+  {
+    "revision": "f17b4c76a17a302785f44318c0b43b88",
+    "url": "/cs185-assignment4/static/media/profilepicture.f17b4c76.jpg"
+  },
+  {
+    "revision": "12f0820c451bdc75f4d1ef97732bf6e8",
+    "url": "/cs185-assignment4/static/media/rw-widgets.12f0820c.woff"
+  },
+  {
+    "revision": "792dcd18baf5f544aabcad1883d673c2",
+    "url": "/cs185-assignment4/static/media/rw-widgets.792dcd18.svg"
+  },
+  {
+    "revision": "bc7c4a59f924cf037aad6e1f9edba366",
+    "url": "/cs185-assignment4/static/media/rw-widgets.bc7c4a59.eot"
+  },
+  {
+    "revision": "eceddf474df95d8d4a7e316668c3be85",
+    "url": "/cs185-assignment4/static/media/rw-widgets.eceddf47.ttf"
+  },
+  {
+    "revision": "89f3d1aad95c0beb3985a56b651ee178",
+    "url": "/cs185-assignment4/static/media/yellow10.89f3d1aa.jpg"
+  },
+  {
+    "revision": "43cf71e11cb78ba7af69e3fdd5be686e",
+    "url": "/cs185-assignment4/static/media/yellow3.43cf71e1.jpg"
+  },
+  {
+    "revision": "95cd469d462050db9ed4922c8e08085f",
+    "url": "/cs185-assignment4/static/media/yellow4.95cd469d.jpg"
+  },
+  {
+    "revision": "1ca8c02287dc7eb7b30738668b1021f4",
+    "url": "/cs185-assignment4/static/media/yellow6.1ca8c022.jpg"
+  },
+  {
+    "revision": "6d8cfb13144fa59263170a7c395b0f1f",
+    "url": "/cs185-assignment4/static/media/yellow9.6d8cfb13.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cs185-assignment4/precache-manifest.ef3581abd2628c234fc71c3c139ab3af.js"
+  "/cs185-assignment4/precache-manifest.f262bf18f8164b3d2416b86b77d267cf.js"
 );
 
 self.addEventListener('message', (event) => {
